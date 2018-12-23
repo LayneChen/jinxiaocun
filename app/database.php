8871a5ec0f391c544aed527fa42157c40dbe75a7
chenlei <chenlei@haitun.hk>
2018-12-24 00:47:29 +0800

    first

---
+++ app/database.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'type' => 'mysql',
+  'hostname' => 'localhost',
+  'database' => 'db',
+  'username' => 'root',
+  'password' => 'root',
+  'hostport' => '3306',
+  'dsn' => '',
+  'params' => 
+  array (
+  ),
+  'charset' => 'utf8',
+  'prefix' => 'w_',
+  'debug' => true,
+  'deploy' => 0,
+  'rw_separate' => false,
+  'master_num' => 1,
+  'slave_no' => '',
+  'fields_strict' => true,
+  'resultset_type' => 'array',
+  'auto_timestamp' => false,
+  'datetime_format' => 'Y-m-d H:i:s',
+  'sql_explain' => false,
+);
